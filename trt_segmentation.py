924d54813ac650e063c04e5e16d2a4739f5e32a6
Romil Aggarwal <romil.aggarwal@research.iiit.ac.in>
2022-01-31 17:14:08 +0530

    Update trt_segmentation.py

--- trt_segmentation.py
+++ trt_segmentation.py
@@ -5,10 +5,12 @@ import glob
 import cv2
 import argparse
 
+import torch
+import torch.nn.functional as F
+
 import util.io
 
 import sys
-import torch
 
 from torchvision.transforms import Compose
 from dpt.models import DPTSegmentationModel
@@ -17,17 +19,18 @@ from dpt.transforms import Resize, NormalizeImage, PrepareForNet
 from loguru import logger
 
 import tensorrt as trt
-import numpy as np 
+import numpy as np
 import common
-import time
-import onnxruntime
+import time 
 
-DTYE = trt.float32
+DTYPE = trt.float32
 TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 trt.init_libnvinfer_plugins(None, "")
+#import shutil
 
 class TensorRTYoloModel:
     def __init__(self, model_path):
+        #self.device = torch.device("cuda")
         self.TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
         self.trt_runtime = trt.Runtime(self.TRT_LOGGER)
         with open(model_path, 'rb') as f:
@@ -78,36 +81,30 @@ def run(input_path, output_path, model_path, model_type="dpt_hybrid", optimize=T
     # create output folder
     os.makedirs(output_path, exist_ok=True)
 
-    #model_path = "../dpt_seg_hyb_171121.trt"
-    #dpt_engine = TensorRTYoloModel(model_path) 
-    #model_path = "/media/31A079936F39FBF9/romil/onnx_cache_trt/dpt_seg_hyb_view_folded_2_161121.onnx"
-    model_path = "/media/31A079936F39FBF9/romil/onnx_cache_trt/dpt_seg_hyb_view_folded_pad_resize_161121.onnx"
-    session = onnxruntime.InferenceSession(model_path)   
+    
+    model_path ="/home/romil/dpt_int8_021221.trt"
+    dpt_engine = TensorRTYoloModel(model_path)  
+
+    source_window = "dpt"  
 
     print("start processing")
+    key = ' '
 
     for ind, img_name in enumerate(img_names):
-
-        print("  processing {} ({}/{})".format(img_name, ind + 1, num_images))
-
-        # input
         img = util.io.read_image(img_name)
         img_input = transform({"image": img})["image"]
 
         # compute
-        ort_inputs = {session.get_inputs()[0].name: img_input[None, :, :, :]}
-        t2 = time.time()
-        out = session.run(None, ort_inputs)
-        t3 = time.time()
+        out = dpt_engine.infer(img_input)
         prediction = out[0].copy()
-        #prediction.resize((150, img.shape[0], img.shape[1]))
-        prediction = torch.from_numpy(prediction)
+        prediction = torch.from_numpy(prediction.reshape((1,150,480,1376)))
         prediction = torch.nn.functional.interpolate(
-                prediction, size=img.shape[:2], mode="bicubic", align_corners=False
-            )
+            prediction, size=img.shape[:2], mode="bicubic", align_corners=False
+        )
+        #prediction.resize((150, img.shape[0], img.shape[1]))
         print("prediction shape", prediction.shape, prediction.dtype)
-        #prediction = np.argmax(prediction, axis=0) + 1
         prediction = torch.argmax(prediction, dim=1) + 1
+        #prediction = np.argmax(prediction, axis=0) + 1
         print("prediction shape after argmax", prediction.shape, prediction.dtype)
         prediction = prediction.squeeze().cpu().numpy()
         # output
@@ -117,6 +114,10 @@ def run(input_path, output_path, model_path, model_type="dpt_hybrid", optimize=T
         #print(prediction)
         #cv2.imwrite("prediction.png", prediction)
         util.io.write_segm_img(filename, img, prediction, alpha=0.5)
+        #out_img = util.io.give_segm_img(img, prediction, alpha=0.5)
+        #cv2.imshow(source_window, out_img)
+
+        #key = cv2.waitKey(1) & 255
 
     print("finished")
 
@@ -155,6 +156,7 @@ if __name__ == "__main__":
 
     if args.model_weights is None:
         args.model_weights = default_models[args.model_type]
+
 
     # compute segmentation maps
     run(
